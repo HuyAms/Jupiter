913530f35346da6004c128717cc2f5092233b8e1
HuyTrinh <huy@makehelsinki.com>
2018-10-07 23:01:52 +0300

    Dragging with UIPanGestureRecognizer

--- Jupiter/Controllers/HomeController.swift
+++ Jupiter/Controllers/HomeController.swift
@@ -15,46 +15,76 @@ class HomeController: UITableViewController {
         tableView.backgroundColor = UIColor.red
         
         setupNavigationItem()
+        
+        setupMenuController()
+        
+        //Pan Gesture
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
     }
+    
+
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        let translation = gesture.translation(in: view)
+        
+        if gesture.state == .changed {
+            var x = translation.x
+            x = min(menuWitdh, x)
+            x = max(0, x)
+            
+            let transform = CGAffineTransform(translationX: x, y: 0)
+            menuController.view.transform = transform
+            navigationController?.view.transform = transform
+        } else if gesture.state == .ended {
+            handleOpen()
+        }
+    }
+    
+    @objc func handleOpen() {
+        
+        performAnimations(tranform: CGAffineTransform(translationX: self.menuWitdh, y: 0))
+    }
+    
+    @objc func handleHide() {
+        
+        performAnimations(tranform: .identity)
+        
+//        menuController.view.removeFromSuperview()
+//        menuController.removeFromParent()
+    }
+    
+    // MARK: - Fileprivate
     
     let menuController = MenuController()
     
     fileprivate let menuWitdh: CGFloat = 300
     
-    @objc func handleOpen() {
-        
-        //Initial position
-        menuController.view.frame = CGRect(x: -menuWitdh, y: 0, width: menuWitdh, height: self.view.frame.height)
-        
-        let mainWindow = UIApplication.shared.keyWindow
-        mainWindow?.addSubview(menuController.view)
+    fileprivate func performAnimations(tranform: CGAffineTransform) {
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             
             //Final position
-            self.menuController.view.transform = CGAffineTransform(translationX: self.menuWitdh, y: 0)
-        }, completion: nil)
-        
-        addChild(menuController)
-        
-    }
-    
-    @objc func handleHide() {
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            
-            //Final position
-            self.menuController.view.transform = .identity
-        }, completion: nil)
-        
-//        menuController.view.removeFromSuperview()
-//        menuController.removeFromParent()
+            self.menuController.view.transform = tranform
+//            self.view.transform = tranform
+            self.navigationController?.view.transform = tranform
+        })
     }
     
     fileprivate func setupNavigationItem() {
         navigationItem.title = "Home"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupMenuController() {
+        //Initial position
+        menuController.view.frame = CGRect(x: -menuWitdh, y: 0, width: menuWitdh, height: self.view.frame.height)
+        
+        let mainWindow = UIApplication.shared.keyWindow
+        mainWindow?.addSubview(menuController.view)
+        addChild(menuController)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
