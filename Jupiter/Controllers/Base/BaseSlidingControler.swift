5f2f92da1b55e86fba4db6bbf4fdde6da2346347
HuyTrinh <huy@makehelsinki.com>
2018-10-08 10:48:20 +0300

    magical autolayout

---
+++ Jupiter/Controllers/Base/BaseSlidingControler.swift
@@ -0,0 +1,73 @@
+//
+//  BaseSlidingControler.swift
+//  Jupiter
+//
+//  Created by HuyTrinh on 08/10/2018.
+//  Copyright © 2018 HuyTrinh. All rights reserved.
+//
+
+import UIKit
+
+class BaseSlidingController: UIViewController {
+    
+    let redView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .red
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+    let blueView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .blue
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+        
+        setupViews()
+        
+        setupPanGesture()
+    }
+
+    fileprivate func setupPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    var redViewLeadingConstraint: NSLayoutConstraint!
+    fileprivate let menuWidth: CGFloat = 300
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        var x = translation.x
+        
+        x = min(menuWidth, x)
+        
+        redViewLeadingConstraint.constant = x
+    }
+    
+    fileprivate func setupViews() {
+        view.addSubview(redView)
+        view.addSubview(blueView)
+        
+        //Auto layout
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.topAnchor),
+            redView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            blueView.topAnchor.constraint(equalTo: view.topAnchor),
+            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor),
+            blueView.widthAnchor.constraint(equalToConstant: menuWidth),
+            blueView.bottomAnchor.constraint(equalTo: redView.bottomAnchor)
+            ])
+        
+        redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        redViewLeadingConstraint.isActive = true
+    }
+}
