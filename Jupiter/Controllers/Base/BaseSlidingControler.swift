d519baefd591fb922d179216b619061b126bbb1b
HuyTrinh <huy@makehelsinki.com>
2018-10-08 11:50:25 +0300

    Home/Menu View and constraint animations

--- Jupiter/Controllers/Base/BaseSlidingControler.swift
+++ Jupiter/Controllers/Base/BaseSlidingControler.swift
@@ -41,14 +41,38 @@ class BaseSlidingController: UIViewController {
     
     var redViewLeadingConstraint: NSLayoutConstraint!
     fileprivate let menuWidth: CGFloat = 300
+    fileprivate var isMenuOpen = false
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: view)
         var x = translation.x
         
-        x = min(menuWidth, x)
+        x = isMenuOpen ? x + menuWidth : x
         
+        x = min(menuWidth, x)
+        x = max(0, x)
+
         redViewLeadingConstraint.constant = x
+        
+        if gesture.state == .ended {
+            handleEnded(gesture: gesture)
+        }
+    }
+    
+    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        if translation.x < menuWidth / 2 {
+            redViewLeadingConstraint.constant = 0
+            isMenuOpen = false
+        } else {
+            redViewLeadingConstraint.constant = menuWidth
+            isMenuOpen = true
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        })
     }
     
     fileprivate func setupViews() {
@@ -62,12 +86,44 @@ class BaseSlidingController: UIViewController {
             redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
             blueView.topAnchor.constraint(equalTo: view.topAnchor),
-            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: redView.safeAreaLayoutGuide.leadingAnchor),
             blueView.widthAnchor.constraint(equalToConstant: menuWidth),
             blueView.bottomAnchor.constraint(equalTo: redView.bottomAnchor)
             ])
         
         redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
         redViewLeadingConstraint.isActive = true
+        
+        setupViewControllers()
+    }
+    
+    fileprivate func setupViewControllers() {
+        //Add HomeControllers into the redView
+        let homeController = HomeController()
+        let menuController = MenuController()
+        
+        let homeView = homeController.view!
+        let menuView = menuController.view!
+
+        homeView.translatesAutoresizingMaskIntoConstraints = false
+        menuView.translatesAutoresizingMaskIntoConstraints = false
+        
+        redView.addSubview(homeView)
+        blueView.addSubview(menuView)
+        
+        NSLayoutConstraint.activate([
+            homeView.topAnchor.constraint(equalTo: redView.topAnchor),
+            homeView.leadingAnchor.constraint(equalTo: redView.leadingAnchor),
+            homeView.bottomAnchor.constraint(equalTo: redView.bottomAnchor),
+            homeView.trailingAnchor.constraint(equalTo: redView.trailingAnchor),
+            
+            menuView.topAnchor.constraint(equalTo: blueView.topAnchor),
+            menuView.leadingAnchor.constraint(equalTo: blueView.leadingAnchor),
+            menuView.bottomAnchor.constraint(equalTo: blueView.bottomAnchor),
+            menuView.trailingAnchor.constraint(equalTo: blueView.trailingAnchor),
+            ])
+        
+        addChild(homeController)
+        addChild(menuController)
     }
 }
--- Jupiter/Controllers/HomeController.swift
+++ Jupiter/Controllers/HomeController.swift
@@ -24,7 +24,7 @@ class HomeController: UITableViewController {
         
         setupMenuController()
         
-        setupPanGesture()
+//        setupPanGesture()
         
         setupDarkCoverView()
     }
